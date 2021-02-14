be2da169bd7f0cec5560b6ee5e5cf677251ead37
Skye <75392025+skyebacus@users.noreply.github.com>
2021-02-14 13:42:41 -0700

    finished ch4 exercises

--- Ch4/shell_start.py
+++ Ch4/shell_start.py
@@ -4,6 +4,8 @@
 import os
 from os import path
 import shutil
+from shutil import make_archive
+from zipfile import ZipFile
 
 def main():
   # make a duplicate of an existing file
@@ -15,17 +17,20 @@ def main():
     dst = src + ".bak"
     
     # copy over the permissions, modification times, and other info
-    shutil.copy(src, dst)
-    shutil.copystat(src, dst)
+    # shutil.copy(src, dst)
+    # shutil.copystat(src, dst)
 
     # rename the original file
-
+    # os.rename("textfile.txt", "newfile.txt")
     
     # now put things into a ZIP archive
-
+    # root_dir, tail = path.split(src)
+    # shutil.make_archive("archive", "zip", root_dir)
 
     # more fine-grained control over ZIP files
-
+    with ZipFile("testzip.zip", "w") as newzip:
+      newzip.write("textfile.txt")
+      newzip.write("textfile.txt.bak")
       
 if __name__ == "__main__":
   main()
