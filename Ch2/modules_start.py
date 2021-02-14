6ae9845a61541691fbdb5d3748d54628c3a397d1
Skye <75392025+skyebacus@users.noreply.github.com>
2021-02-13 19:31:23 -0700

    finishing Ch2 exercises

--- Ch2/modules_start.py
+++ Ch2/modules_start.py
@@ -1,10 +1,13 @@
 # import the math module, which contains features for working with mathematics
-
+import math
 
 # the math module contains lots of pre-built functions
-
+print("The square root of 16 is", math.sqrt(16))
 
 # in addition to functions, some modules contain useful constants
-
+print("Pi is:", math.pi)
 
 # try some of the math functions for yourself here:
+arr = [1, 2, 3, 4, 5]
+product = math.prod(arr, start = 2)
+print(product)
