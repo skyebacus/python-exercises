b6a85fc9e5c69bdd20b78458fe6e6a133c25757f
Skye <75392025+skyebacus@users.noreply.github.com>
2021-02-14 13:33:17 -0700

    working on ch4

--- Ch4/shell_start.py
+++ Ch4/shell_start.py
@@ -3,19 +3,21 @@
 #
 import os
 from os import path
+import shutil
 
 def main():
   # make a duplicate of an existing file
   if path.exists("textfile.txt"):
     # get the path to the file in the current directory
-
+    src = path.realpath("textfile.txt")
     
     # let's make a backup copy by appending "bak" to the name
-
+    dst = src + ".bak"
     
     # copy over the permissions, modification times, and other info
+    shutil.copy(src, dst)
+    shutil.copystat(src, dst)
 
-    
     # rename the original file
 
     
